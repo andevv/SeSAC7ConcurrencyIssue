8897f38c0a9e59cedad79ab9f9163dbdf6e4b4f5
andev <andh1006@naver.com>
2025-11-20 10:08:07 +0900

    mutex, semaphore 코드 실습

--- SeSACConcurrencyIssue/ViewController.swift
+++ SeSACConcurrencyIssue/ViewController.swift
@@ -7,13 +7,70 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    var nickname = "고래밥"
+    
+    //mutex
+    let nicknameLock = NSLock()
+    
+    //semaphore
+    let nicknameSemaphore = DispatchSemaphore(value: 1)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //datarace()
+        //mutex()
+        semaphore()
     }
 
-
+    func datarace() {
+        
+        print(">>", nickname)
+        DispatchQueue.global().async {
+            self.nickname = "칙촉"
+        }
+        
+        DispatchQueue.global().async {
+            self.nickname = "카스타드"
+        }
+        print(">>>>", nickname)
+        
+    }
+    
+    func mutex() {
+        
+        print(">>", nickname)
+        DispatchQueue.global().async {
+            self.nicknameLock.lock()
+            self.nickname = "칙촉"
+            self.nicknameLock.unlock()
+        }
+        
+        DispatchQueue.global().async {
+            self.nicknameLock.lock()
+            self.nickname = "카스타드"
+            self.nicknameLock.unlock()
+        }
+        print(">>>>", nickname)
+        
+    }
+    
+    func semaphore() {
+        
+        print(">>", nickname)
+        DispatchQueue.global().async {
+            self.nicknameSemaphore.wait()
+            self.nickname = "칙촉"
+            self.nicknameSemaphore.signal()
+        }
+        
+        DispatchQueue.global().async {
+            self.nickname = "카스타드"
+        }
+        print(">>>>", nickname)
+        
+    }
 }
 

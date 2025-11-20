5211ff72920675ca7e57067f67975ac088d0b26a
andev <andh1006@naver.com>
2025-11-20 11:50:26 +0900

    consume, consuming, borrowing

--- SeSACConcurrencyIssue/OwnershipViewController.swift
+++ SeSACConcurrencyIssue/OwnershipViewController.swift
@@ -7,6 +7,21 @@
 
 import UIKit
 
+/// Swift 2.3
+/// Swift 3
+/// Swift 4+ Codable
+/// Swift 5 ABI
+/// Swift 5.1 (SwiftUI)
+///  - Opaque Type / Property Wrapper
+/// Swift 5.5 (Concurrency)
+///  - Async/Await, actor, sendable
+/// Swift 5.9
+///  - noncopyable / ownership
+/// Swift 6
+///  - 컴파일 타임에 동시성 이슈 해결
+///  - @preconcurrency
+///  - nonisolated
+
 /// Copyable: Swift 5.9+ 값 타입의 복사를 정의
 /// 값 타입에는 Copyable Protocol이 이미 정의되어 있음
 
@@ -24,14 +39,54 @@ struct User2: ~Copyable {
     }
 }
 
+struct Person: ~Copyable {
+    let name: String
+    let age: Int
+    
+    func test() {
+        print(#function)
+    }
+    
+    consuming func test2() {
+        print(#function)
+    }
+    
+    deinit {
+        print("Person Deinit")
+    }
+}
+
 class OwnershipViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchUser()
+        //fetchUser()
+        fetchPerson()
         print("viewDidLoad End")
-
         
+    }
+    
+    func fetchMovePerson() {
+        let person = Person(name: "jack", age: 11)
+        print(person.name)
+        
+        let newPerson = consume person // 소유권 이동
+        print(newPerson.name)
+        
+        introduce(test: newPerson) // 소유권을 넘겨보자 -> 소유권에 대한 이동 없이 참조만, 읽기만
+        
+        print(newPerson.name) // 매개변수 test로 소유권이 넘어가버려 newPerson 사용 불가
+    }
+    
+    func introduce(test: borrowing Person) {
+        print("이름은 \(test.name)이고 나이는 \(test.age)세 입니다.")
+    }
+    
+    func fetchPerson() {
+        let person = Person(name: "Jack", age: 11)
+        print(person.name)
+        print(person.test2())
+        print(#function, person.name)
     }
     
     func fetchUser() {
@@ -41,11 +96,10 @@ class OwnershipViewController: UIViewController {
         var jackname = nickname
         newName = "123"
         
-        
         let user = User2(name: "Jack", age: 22)
         print(user.name)
         
-        let newUser = user
+        let newUser = consume user // 소유권 이동
         print(newUser.name)
         //print(user.name)
     }
